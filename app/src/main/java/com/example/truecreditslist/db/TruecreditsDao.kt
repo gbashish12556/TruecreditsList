4d816681168061f343227b3828c5c43d30bdd4db
Ashish Lattukids <Ashish Gupta>
2021-12-05 17:27:40 +0530

    added db and dao

--- app/src/main/java/com/example/truecreditslist/db/TruecreditsDao.kt
+++ app/src/main/java/com/example/truecreditslist/db/TruecreditsDao.kt
@@ -1,4 +1,19 @@
 package com.example.truecreditslist.db
 
-class TruecreditsDao {
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.truecreditslist.data.TruecreditsPost
+
+@Dao
+interface TruecreditsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(posts: List<TruecreditsPost>)
+
+    @Query("SELECT * FROM truecredit_post WHERE name = :name ORDER BY indexInResponse ASC")
+    fun postsBySubreddit(name: String): PagingSource<Int, TruecreditsPost>
+
 }
--- app/src/main/java/com/example/truecreditslist/db/TruecreditsDb.kt
+++ app/src/main/java/com/example/truecreditslist/db/TruecreditsDb.kt
@@ -1,4 +1,26 @@
 package com.example.truecreditslist.db
 
-class TruecreditsDb {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.truecreditslist.data.TruecreditsPost
+
+
+@Database(
+    entities = [TruecreditsPost::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TruecreditsDb : RoomDatabase() {
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): TruecreditsDb {
+            val databaseBuilder = Room.databaseBuilder(context, TruecreditsDb::class.java, "truecredits.db")
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
+    abstract fun posts(): TruecreditsDao
 }
